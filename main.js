8ba05a6bbab76f6b96b6ac736c40e46a9343cdea
3elinda <belinda.cedillo_santiago24@student.cart.org>
2025-03-08 16:56:18 -0800

    slower-increasing size balls

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 // setup canvas
 
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// width and height are set up to match browser viewport
+// ctx allows to draw on canvas
 
 // function to generate random number
 
@@ -22,84 +25,91 @@ function Ball(x, y, velX, velY, color, size) {
   this.size = size;
 }
 
-Ball.prototype.draw = function() {
+Ball.prototype.draw = function () {
   ctx.beginPath();
   ctx.fillStyle = this.color;
   ctx.arc(this.x, this.y, this.size, 0, 1 * Math.PI);
   ctx.fill();
-}
+};
 
-let testBall = new Ball(50, 100, 4, 4, 'blue', 10);
+let testBall = new Ball(50, 100, 4, 4, "blue", 10);
 
-testBall.x
-testBall.size
-testBall.color
+testBall.x;
+testBall.size;
+testBall.color;
 
-testBall.draw()
+testBall.draw();
 
-Ball.prototype.update = function() {
-  if ((this.x + this.size) >= width) {
-    this.velX = -(this.velX);
+Ball.prototype.update = function () {
+  if (this.x + this.size >= width) {
+    this.velX = -this.velX;
+    this.size += 0.1;
   }
-  if ((this.x - this.size) <= 0) {
-    this.velX = -(this.velX);
+  if (this.x - this.size <= 0) {
+    this.velX = -this.velX;
+    this.size += 0.1;
   }
-
-  if ((this.y + this.size) >= height) {
-    this.velY = -(this.velY);
+  if (this.y + this.size >= height) {
+    this.velY = -this.velY;
+    this.size += 0.1;
   }
-
-  if ((this.y - this.size) <= 0) {
-    this.velY = -(this.velY);
+  if (this.y - this.size <= 0) {
+    this.velY = -this.velY;
+    this.size += 0.1;
   }
 
   this.x += this.velX;
   this.y += this.velY;
+};
 
-  Ball.prototype.collisionDetect = function() {
-    for (let j = 0; j < balls.length; j++) {
-      if (!(this === balls[j])) {
-        const dx = this.x - balls[j].x;
-        const dy = this.y - balls[j].y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
-  
-        if (distance < this.size + balls[j].size) {
-          balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
-        }
+Ball.prototype.collisionDetect = function () {
+  for (let j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color =
+          "rgb(" +
+          random(0, 255) +
+          "," +
+          random(0, 255) +
+          "," +
+          random(0, 255) +
+          ")";
       }
     }
-  
-}
-}
+  }
+};
 
-let balls =[];
+let balls = [];
 
 while (balls.length < 65) {
-  let size = random(10,20);
-  let ball = new Ball (
-    random(0 + size,width - size),
-    random(0 + size,height - size),
-    random(-7,7),
-    random(-7,7),
-    'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+  let size = random(10, 20);
+  let ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-2, 2),
+    random(-2, 2),
+    "rgb(" + random(0, 255) + "," + random(0, 255) + "," + random(0, 255) + ")",
     size
   );
 
   balls.push(ball);
-
 }
 
 function loop() {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.20)';
+  ctx.fillStyle = "rgba(0, 0, 0, 0.20)";
   ctx.fillRect(0, 0, width, height);
 
   for (let i = 0; i < balls.length; i++) {
     balls[i].draw();
     balls[i].update();
-    balls[i]. collisionDetect();
+    balls[i].collisionDetect();
   }
 
   requestAnimationFrame(loop);
 }
 
-loop()
+loop();
